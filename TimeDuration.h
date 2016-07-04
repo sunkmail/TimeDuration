11f678d112bde08a7106dadf6b1df77244b8a579
Scott <sunkmail@gmail.com>
2016-07-04 05:14:47 -0700

    Create TimeDuration.h
    First Attempt - Don't Download - Not ready yet!!

---
+++ TimeDuration.h
@@ -0,0 +1,32 @@
+#ifndef TIMEDURATION_H
+#define TIMEDURATION_H
+ 
+#include <Arduino.h>
+
+/* Library Limitation:    Cannot (currently) do longer than ~45 Days
+ *                        (uses millis() function) 
+ *
+ */
+
+
+class TimeDuration{
+  
+  public:
+  TimeDuration();
+  ~TimeDuration();
+  void startTime();
+  doTime(int duration);
+  String showTime(byte duration, char first, char last);  //see .cpp file for 'char' usage
+  unsigned long makeTime(int setDuration);
+
+ 
+  Private:
+  unsigned long timeStart;
+  int duration[4];
+  int setDuration[4];  
+  unsigned long makeMillis;
+
+};
+
+
+#endif
